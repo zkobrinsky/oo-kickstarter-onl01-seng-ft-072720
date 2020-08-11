8422fa54a135eef36f712dc3b33d3439df012d21
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-11 19:02:24 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,11 +2,13 @@ class Project
 
 attr_reader :title, :backers
 
-
-
 def initialize(title)
   @title = title
   @backers = []
+end
+
+def add_backer(backer_obj)
+  @backers << backer_obj
 end
 
 end
